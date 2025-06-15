aff5c7d3167783d3cba2f9870610ebf06e5d878a
maratmartypasha <maratmartypasha@gmail.com>
2025-06-15 21:19:09 +0700

    update

--- app/core/config.php
+++ app/core/config.php
@@ -1,25 +1,24 @@
-<?php 
+<?php
 
-if($_SERVER['SERVER_NAME'] == "localhost")
-{
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+    // Local development
+    define("ROOT", "http://localhost/music_website/public");
 
-	//for local server
-	define("ROOT", "http://localhost/music_website/public");
+    define("DBDRIVER", "mysql");
+    define("DBHOST", "localhost");
+    define("DBUSER", "root");
+    define("DBPASS", "");
+    define("DBNAME", "music_db");
+    define("DBPORT", "3306");
 
-	define("DBDRIVER", "mysql");
-	define("DBHOST", "localhost");
-	define("DBUSER", "root");
-	define("DBPASS", "");
-	define("DBNAME", "music_db");
+} else {
+    // Railway deployment
+    define("ROOT", "https://" . $_SERVER['HTTP_HOST']); // gunakan domain Railway otomatis
 
-}else{
-
-	//for online server
-	define("ROOT", "http://www.mywebsite.com");	
-
-	define("DBDRIVER", "mysql");
-	define("DBHOST", "localhost");
-	define("DBUSER", "root");
-	define("DBPASS", "");
-	define("DBNAME", "music_db");
+    define("DBDRIVER", "mysql");
+    define("DBHOST", getenv("DB_HOST"));
+    define("DBUSER", getenv("DB_USER"));
+    define("DBPASS", getenv("DB_PASS"));
+    define("DBNAME", getenv("DB_NAME"));
+    define("DBPORT", getenv("DB_PORT"));
 }
