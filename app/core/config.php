4b811acd63bd565c7dc23aab22f9618e5f32f4a4
maratmartypasha <maratmartypasha@gmail.com>
2025-06-15 22:20:37 +0700

    update

--- app/core/config.php
+++ app/core/config.php
@@ -1,7 +1,6 @@
 <?php
 
 if ($_SERVER['SERVER_NAME'] == "localhost") {
-    // Local development
     define("ROOT", "http://localhost/music_website/public");
 
     define("DBDRIVER", "mysql");
@@ -12,14 +11,36 @@ if ($_SERVER['SERVER_NAME'] == "localhost") {
     define("DBPORT", "3306");
 
 } else {
-    // Railway deployment
-    define("ROOT", "https://" . $_SERVER['HTTP_HOST']); // gunakan domain Railway otomatis
+    define("ROOT", "https://" . $_SERVER['HTTP_HOST']);
 
     define("DBDRIVER", "mysql");
     define("DBHOST", getenv("MYSQLHOST"));
-	define("DBUSER", getenv("MYSQLUSER"));
-	define("DBPASS", getenv("MYSQLPASSWORD"));
-	define("DBNAME", getenv("MYSQLDATABASE"));
-	define("DBPORT", getenv("MYSQLPORT"));
+    define("DBUSER", getenv("MYSQLUSER"));
+    define("DBPASS", getenv("MYSQLPASSWORD"));
+    define("DBNAME", getenv("MYSQLDATABASE"));
+    define("DBPORT", getenv("MYSQLPORT"));
+}
 
+// PDO connection
+$host = DBHOST;
+$port = DBPORT;
+$dbname = DBNAME;
+$user = DBUSER;
+$pass = DBPASS;
+
+$dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8";
+
+$tries = 5;
+while ($tries > 0) {
+    try {
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        break;
+    } catch (PDOException $e) {
+        $tries--;
+        if ($tries == 0) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+        sleep(2);
+    }
 }
